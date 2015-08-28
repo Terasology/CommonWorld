dd3d4d0aae73aab7dee78d6ddc90c571503c1b4a
Martin Steiger <git@martin-steiger.de>
2015-08-28 19:42:01 +0200

    Update BoundingBox for new Vector classes

--- src/main/java/org/terasology/commonworld/geom/BoundingBox.java
+++ src/main/java/org/terasology/commonworld/geom/BoundingBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 MovingBlocks
+ * Copyright 2015 MovingBlocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,20 +19,20 @@ package org.terasology.commonworld.geom;
 import java.awt.Rectangle;
 import java.util.Collection;
 
-import org.terasology.math.Vector2i;
+import org.terasology.math.geom.BaseVector2i;
+import org.terasology.math.geom.Rect2i;
 
 import com.google.common.base.Optional;
 
 /**
- * Defines a axis-aligned bounding box based on a set of Vector2is 
- * @author Martin Steiger
+ * Defines a axis-aligned bounding box based on a set of Vector2is
  */
 public class BoundingBox {
     private int x1;
     private int y1;
     private int x2;
     private int y2;
-    
+
     private boolean empty = true;
 
     /**
@@ -46,53 +46,53 @@ public class BoundingBox {
     }
 
     /**
-     * @param pt the initial Vector2i 
+     * @param pt the initial Vector2i
      */
-    public BoundingBox(Vector2i pt) {
-        x1 = pt.x;
-        y1 = pt.y;
-        x2 = pt.x;
-        y2 = pt.y;
-        
+    public BoundingBox(BaseVector2i pt) {
+        x1 = pt.getX();
+        y1 = pt.getY();
+        x2 = pt.getX();
+        y2 = pt.getY();
+
         empty = false;
     }
 
     /**
-     * @param pts the initial Vector2is 
+     * @param pts the initial Vector2is
      */
-    public BoundingBox(Collection<Vector2i> pts) {
+    public BoundingBox(Collection<? extends BaseVector2i> pts) {
         this();
         addAll(pts);
     }
-    
+
     /**
      * @param pts a collection of Vector2is
      * @return the bounding rectangle of pts or absent() if pts is empty
      */
-    public static Optional<Rectangle> getBoundingRect(Collection<Vector2i> pts) {
+    public static Optional<Rectangle> getBoundingRect(Collection<? extends BaseVector2i> pts) {
         if (pts.isEmpty()) {
             return Optional.absent();
         }
-        
+
         BoundingBox bbox = new BoundingBox(pts);
         return Optional.of(bbox.toRectangle());
     }
-    
+
     /**
      * Resizes to include pt
      * @param pt the Vector2i
      */
-    public void add(Vector2i pt) {
-        add(pt.x, pt.y);
+    public void add(BaseVector2i pt) {
+        add(pt.getX(), pt.getY());
     }
 
     /**
      * Resizes to include all Vector2is
      * @param pts a collection of Vector2is
      */
-    public void addAll(Collection<Vector2i> pts) {
-        for (Vector2i pt : pts) {
-            add(pt.x, pt.y);
+    public void addAll(Collection<? extends BaseVector2i> pts) {
+        for (BaseVector2i pt : pts) {
+            add(pt.getX(), pt.getY());
         }
     }
 
@@ -129,13 +129,24 @@ public class BoundingBox {
     }
 
     /**
+     * @return a new Rect2i instance containing the bounds
+     */
+    public Rect2i toRect2i() {
+        if (empty) {
+            throw new IllegalStateException("BoundingBox undefined - no Points were added");
+        }
+
+        return Rect2i.createFromMinAndMax(x1, y1, x2, y2);
+    }
+
+    /**
      * @return a new Rectangle containing the bounds
      */
     public Rectangle toRectangle() {
         if (empty) {
             throw new IllegalStateException("BoundingBox undefined - no Points were added");
         }
-        
+
         return new Rectangle(x1, y1, x2 - x1, y2 - y1);
     }
 
