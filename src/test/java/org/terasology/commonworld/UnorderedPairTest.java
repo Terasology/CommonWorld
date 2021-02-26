3ab6c81a04989e573a8d6103812ad0920406fe0c
Michael Pollind <mpollind@gmail.com>
2021-02-26 12:57:44 -0800

    test: update test for junit5 (#10)

--- src/test/java/org/terasology/commonworld/UnorderedPairTest.java
+++ src/test/java/org/terasology/commonworld/UnorderedPairTest.java
@@ -16,26 +16,28 @@
 
 package org.terasology.commonworld;
 
-import org.junit.Assert;
-import org.junit.Test;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 /**
  * Tests the {@link UnorderedPair} class.
  */
 public class UnorderedPairTest {
     @Test
     public void equalsTest()  {
-        Assert.assertEquals(new UnorderedPair<>(0, 0), new UnorderedPair<>(0, 0));
-        Assert.assertEquals(new UnorderedPair<>(1, 1), new UnorderedPair<>(1, 1));
-        Assert.assertEquals(new UnorderedPair<>(1, 13), new UnorderedPair<>(1, 13));
-        Assert.assertEquals(new UnorderedPair<>(1, 13), new UnorderedPair<>(13, 1));
+        assertEquals(new UnorderedPair<>(0, 0), new UnorderedPair<>(0, 0));
+        assertEquals(new UnorderedPair<>(1, 1), new UnorderedPair<>(1, 1));
+        assertEquals(new UnorderedPair<>(1, 13), new UnorderedPair<>(1, 13));
+        assertEquals(new UnorderedPair<>(1, 13), new UnorderedPair<>(13, 1));
     }
 
     @Test
     public void notEqualsTest() {
-        Assert.assertNotEquals(new UnorderedPair<>(1, 1), new UnorderedPair<>(0, 1));
-        Assert.assertNotEquals(new UnorderedPair<>(0, 13), new UnorderedPair<>(0, 1));
-        Assert.assertNotEquals(new UnorderedPair<>(0, 13), new Object());
-        Assert.assertNotEquals(new UnorderedPair<>(0, 13), null);
+        assertNotEquals(new UnorderedPair<>(1, 1), new UnorderedPair<>(0, 1));
+        assertNotEquals(new UnorderedPair<>(0, 13), new UnorderedPair<>(0, 1));
+        assertNotEquals(new UnorderedPair<>(0, 13), new Object());
+        assertNotEquals(new UnorderedPair<>(0, 13), null);
     }
 }
